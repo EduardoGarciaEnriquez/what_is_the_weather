2f8e6361e371d47fa441d300ef4352533b15f80f
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-04-17 15:21:07 -0600

    ref: javascript for deploy refactor.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # WhatSTheWeather
-Weather forecast that gives the weather, sky, temperature and wind speed of whichever city in the world the user asks for. Developed with HTML5, CSS3, Bootstrap4, PHP and “openweathermap.org” API.
+Weather forecast that gives the weather, sky, temperature and wind speed of whichever city in the world the user asks for. Developed with HTML5, CSS3, Bootstrap4, JavaScript and “openweathermap.org” API.
---
+++ index.js
@@ -0,0 +1,38 @@
+const display = document.querySelector("#weather");
+const input = document.querySelector("#city");
+const form = document.querySelector('#form');
+
+async function searchWeather(city) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=0853d40341443e2e1cbdba1914754e60`
+  );
+  const data = await response.json();
+
+  if (data?.cod === 200) {
+    const city = data.name;
+    const temp = (data["main"]["temp"] - 273.15).toFixed() + "°C";
+    const wind = data["wind"]["speed"] + " m/s";
+    const description = data["weather"][0]["description"];
+    display.innerHTML = `
+    <div class="alert alert-primary" role="alert">
+    The weather in ${city} is currently ${description}. <br/>
+    The temperature is ${temp} <br/>
+    The wind speed is ${wind}
+    </div>
+    `;
+  } else {
+    display.innerHTML =
+      '<div class="alert alert-warning" role="alert">Error: not a valid city</div>';
+  }
+}
+
+function onSubmit(event) {
+  searchWeather(input.value);
+  event.preventDefault();
+}
+
+input?.addEventListener("change", (event) => {
+  input.value = event.target.value;
+});
+
+form?.addEventListener("submit", onSubmit);
