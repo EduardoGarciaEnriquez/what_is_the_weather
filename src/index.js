7322a75bfe0f11780e04a733aa34d2530f88c39a
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-05-31 06:47:54 -0600

    Refactor[index.js]: refactor functions TDD

--- src/index.js
+++ src/index.js
@@ -1,29 +1,53 @@
 const display = document.querySelector("#weather");
 const input = document.querySelector("#city");
-const form = document.querySelector('#form');
-const API_KEY= '0853d40341443e2e1cbdba1914754e60'
+const form = document.querySelector("#form");
 
-async function searchWeather(city) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
-  );
-  const data = await response.json();
+function returnLoading() {
+  return (display.innerHTML = `<div class="spinner-border text-primary mt-4" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>`);
+}
 
-  if (data?.cod === 200) {
-    const city = data.name;
-    const temp = (data["main"]["temp"] - 273.15).toFixed() + "°C";
-    const wind = data["wind"]["speed"] + " m/s";
-    const description = data["weather"][0]["description"];
-    display.innerHTML = `
+function returnWeather({ city, description, temp, wind }) {
+  return (display.innerHTML = `
     <div class="alert alert-primary" role="alert">
     The weather in ${city} is currently ${description}. <br/>
     The temperature is ${temp} <br/>
     The wind speed is ${wind}
     </div>
-    `;
-  } else {
-    display.innerHTML =
-      '<div class="alert alert-warning" role="alert">Error: not a valid city</div>';
+  `);
+}
+
+function returnError() {
+  return (display.innerHTML = '<div class="alert alert-warning" role="alert">Error: not a valid city</div>');
+}
+
+async function fetchData(city) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`);
+  const data = await response.json();
+  return data
+}
+
+async function searchWeather(city) {
+  //query
+  try {
+    //loading effect
+    returnLoading();
+
+    const data = await fetchData(city);
+
+    if (data?.cod === 200) {
+      const city = data.name;
+      const temp = (data["main"]["temp"] - 273.15).toFixed() + "°C";
+      const wind = data["wind"]["speed"] + " m/s";
+      const description = data["weather"][0]["description"];
+      returnWeather({ city, temp, wind, description });
+    } else {
+      returnError();
+    }
+  } catch (error) {
+    // display.innerHTML = `<div class="alert alert-warning" role="alert">${error}</div>`;
+    console.log(error);
   }
 }
 
@@ -37,3 +61,5 @@ input?.addEventListener("change", (event) => {
 });
 
 form?.addEventListener("submit", onSubmit);
+
+module.exports = searchWeather;
