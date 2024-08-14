f3483fd5f7eab390d4d4d60e2161af68d5967264
Eduardo Enriquez <garciaenriquez.carloseduardo@gmail.com>
2024-08-14 21:06:09 +0000

    feat: functional app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import { useTheme } from './hooks/useTheme'
+import { useForecastWeather } from './hooks/useForecastWeather'
 
+import Loader from './components/loader'
+import Alert from './components/alert'
 import sunIcon from './assets/sun.svg'
 import moonIcon from './assets/moon.svg'
+
+import './App.css'
 
 export const Switch = () => {
   const { theme, handleChange } = useTheme()
@@ -17,17 +22,23 @@ export const Switch = () => {
 function App() {
   const [city, setCity] = useState<string>('')
 
+  const { data, error, loading, getWeather } = useForecastWeather(city)
+
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault()
+    getWeather()
   }
 
+  const displayError = error && !loading
+  const displayData = data && !loading
+
   return (
-    <div className='container'>
+    <>
       <header>
         <h1>What's the weather</h1>
         <Switch />
       </header>
-      <form id="form">
+      <form>
         <label htmlFor="city">Enter the name of the city:</label>
         <input
           type="search"
@@ -37,9 +48,11 @@ function App() {
           name="city"
           value={city}
           onChange={(e) => setCity(e.target.value)}
+          disabled={loading}
         />
 
         <button
+          disabled={loading}
           onClick={handleSubmit}
           type="submit"
           className="btn btn-primary"
@@ -47,13 +60,25 @@ function App() {
           Submit
         </button>
 
-        <div id="weather" className="alert info">
-          <span>The weather in Guadalajara is currently broken clouds.</span>
-          <span>The temperature is 27°C</span>
-          <span>The wind speed is 5.82 m/s</span>
-        </div>
+        {displayData && (
+          <Alert role="info">
+            <span>
+              The weather in {data.city} is currently {data.description}.
+            </span>
+            <span>The temperature is {data.temp}</span>
+            <span>The wind speed is {data.wind}</span>
+          </Alert>
+        )}
+
+        {displayError && (
+          <Alert role="warning">
+            <span>{error}</span>
+          </Alert>
+        )}
+
+        {loading && <Loader />}
       </form>
-    </div>
+    </>
   )
 }
 
